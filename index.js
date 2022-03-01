b2a151a29f076bf6d35b0633c8650219b380a7ff
iGerman <igermanproduction@gmail.com>
2022-03-02 02:33:23 +0500

    Target based on profile list, lowercase window names and profiles, add config file, change logic

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
 const activeWindow = require('active-win');
 const { Client } = require("openrgb-sdk")
 
-const frequency_ms = 1000; //title update frequency
-const target_names = ['Vivaldi.exe', 'Discord', 'Plex.exe'] //window names that will be targeted to try to switch to the profile with the same name
-
-const enableDefaultProfile = true; //Will switch to default profile if no profile for given window
-const defaultProfile = 'a'; //said default profile
-
-let currentProfile;
-let windowName;
+const {update_frequency, enableDefaultProfile, defaultProfileName, server_port, server_host} = require('./config.json');
 
 async function start () {
-	const client = new Client("Example", 6742, "localhost")
+	const client = new Client("openrgb-title-switcher", server_port, server_host)
 	
 	await client.connect()
     const profileList = await client.getProfileList();
     console.log('List of profiles: ', profileList);
 
+    let windowName = '';
+    let currentProfile = '';
+
     setInterval(async () => {
-        
-        let win = await activeWindow({});
-        if (win.owner.name !== windowName) {
-            console.log('Current window name: ' + win.owner.name);
-            windowName = win.owner.name;
+
+        const win = await activeWindow({});
+        if (win.owner.name.toLocaleLowerCase() !== windowName.toLocaleLowerCase()) {
+            console.log('Current window name: ' + win.owner.name.toLocaleLowerCase());
+            windowName = win.owner.name.toLocaleLowerCase();
         }
-        
-        profileList.some(p => {
-            if (p == windowName && target_names.some(t => t == windowName) && currentProfile !== p) {
-                console.log('Setting profile to: ' + p);
-                currentProfile = p;
-                client.loadProfile(currentProfile);
-            }
-            if (!(p == windowName || target_names.some(t => t == windowName)) && enableDefaultProfile && currentProfile !== defaultProfile) {
-                console.log('Setting profile to default')
-                currentProfile = defaultProfile;
-                client.loadProfile(currentProfile);
+
+        const isValidProfile = profileList.some(p => {
+            p = p.toLocaleLowerCase();
+            if (p == windowName) { 
+                if (currentProfile !== p) {
+                    currentProfile = p;
+                    console.log('Setting profile: ' + currentProfile)
+                    client.loadProfile(currentProfile);
+                }
+                return true;
             }
         });
-      }, frequency_ms);
+
+        if (!isValidProfile && enableDefaultProfile && currentProfile !== defaultProfileName) {
+            console.log('Setting default profile');
+            currentProfile = defaultProfileName;
+            client.loadProfile(currentProfile);
+        };
+
+      }, update_frequency);
 
       [`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `SIGTERM`].forEach((eventType) => {
         process.on(eventType, function () {
-            client.loadProfile(defaultProfile);
+            client.loadProfile(defaultProfileName);
             process.exit();
         });
       })
