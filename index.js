8e8245d9b40243f963b97e6d60053079850d20eb
iGerman <igermanproduction@gmail.com>
2022-03-23 23:24:03 +0500

    Startup delay + formatting + handle ECONNREFUSED (?)

--- index.js
+++ index.js
@@ -1,52 +1,86 @@
-const activeWindow = require('active-win');
-const { Client } = require("openrgb-sdk")
+const activeWindow = require("active-win");
+const { Client } = require("openrgb-sdk");
 
-const {update_frequency, enableDefaultProfile, defaultProfileName, server_port, server_host} = require('./config.json');
+const {
+  update_frequency,
+  enableDefaultProfile,
+  defaultProfileName,
+  server_port,
+  server_host,
+  start_delay_ms,
+} = require("./config.json");
 
-async function start () {
-	const client = new Client("openrgb-title-switcher", server_port, server_host)
-	
-	await client.connect()
-    const profileList = await client.getProfileList();
-    console.log('List of profiles: ', profileList);
+async function start() {
+  const client = new Client("openrgb-title-switcher", server_port, server_host);
 
-    let windowName = '';
-    let currentProfile = '';
+  try {
+    await client.connect();
+  } catch (error) {
+    if (error.message.includes("ECONNREFUSED")) {
+      console.log(
+        "Connection refused. If OpenRGB is running and the server is started, check configuration"
+      );
+      return;
+    }
+    console.log(error);
+    return;
+  }
 
-    setInterval(async () => {
+  const profileList = await client.getProfileList();
+  console.log("List of profiles: ", profileList);
 
-        const win = await activeWindow({});
-        if (win.owner.name.toLocaleLowerCase() !== windowName.toLocaleLowerCase()) {
-            console.log('Current window name: ' + win.owner.name.toLocaleLowerCase());
-            windowName = win.owner.name.toLocaleLowerCase();
+  let windowName = "";
+  let currentProfile = "";
+
+  setInterval(async () => {
+    const win = await activeWindow({});
+    if (win.owner.name.toLocaleLowerCase() !== windowName.toLocaleLowerCase()) {
+      console.log("Current window name: " + win.owner.name.toLocaleLowerCase());
+      windowName = win.owner.name.toLocaleLowerCase();
+    }
+
+    const isValidProfile = profileList.some((p) => {
+      p = p.toLocaleLowerCase();
+      if (p == windowName) {
+        if (currentProfile !== p) {
+          currentProfile = p;
+          console.log("Setting profile: " + currentProfile);
+          client.loadProfile(currentProfile);
         }
+        return true;
+      }
+    });
 
-        const isValidProfile = profileList.some(p => {
-            p = p.toLocaleLowerCase();
-            if (p == windowName) { 
-                if (currentProfile !== p) {
-                    currentProfile = p;
-                    console.log('Setting profile: ' + currentProfile)
-                    client.loadProfile(currentProfile);
-                }
-                return true;
-            }
-        });
+    if (
+      !isValidProfile &&
+      enableDefaultProfile &&
+      currentProfile !== defaultProfileName
+    ) {
+      console.log("Setting default profile");
+      currentProfile = defaultProfileName;
+      client.loadProfile(currentProfile);
+    }
+  }, update_frequency);
 
-        if (!isValidProfile && enableDefaultProfile && currentProfile !== defaultProfileName) {
-            console.log('Setting default profile');
-            currentProfile = defaultProfileName;
-            client.loadProfile(currentProfile);
-        };
-
-      }, update_frequency);
-
-      [`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `SIGTERM`].forEach((eventType) => {
-        process.on(eventType, function () {
-            client.loadProfile(defaultProfileName);
-            process.exit();
-        });
-      })
+  [`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `SIGTERM`].forEach((eventType) => {
+    process.on(eventType, function () {
+      client.loadProfile(defaultProfileName);
+      process.exit();
+    });
+  });
+  process.on("uncaughtException", (err) => {
+    console.error(err);
+  });
 }
 
-start()
+(function repeat() {
+  if (start_delay_ms !== 0)
+    console.log("Starting in " + start_delay_ms + " ms");
+  setTimeout(() => {
+    try {
+      start();
+    } catch (error) {
+      repeat();
+    }
+  }, start_delay_ms); //startup delay to allow for openrgb to autostart
+})();
